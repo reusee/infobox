768eee43dceb14d3f00753724f7bba9503e5c072
reus <reusee@gmail.com>
2014-11-14 15:07:24 +0800

    add chest server

---
+++ chest/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/gob"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os/user"
+	"path/filepath"
+
+	infobox "../"
+	"github.com/reusee/gobchest"
+)
+
+func init() {
+	go http.ListenAndServe("localhost:2801", nil)
+
+	gob.Register(new(infobox.Item))
+	gob.Register(new([]*infobox.Item))
+}
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filePath := filepath.Join(user.HomeDir, ".chest.gob")
+	server, err := gobchest.NewServer("localhost:2800", filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+	<-make(chan bool)
+}
